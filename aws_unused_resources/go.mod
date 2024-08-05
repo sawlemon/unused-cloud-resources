f04f9f06bde9982f59fe015d31526f971fa02db7
solomonraj-a-presidio <solomonraj.a@presidio.com>
2024-08-05 20:14:46 +0530

    aws_unused_resources package created

---
+++ aws_unused_resources/go.mod
@@ -0,0 +1,14 @@
+module github.com/sawlemon/unused-cloud-resources/aws_unused_resources
+
+go 1.22.5
+
+require github.com/aws/aws-sdk-go-v2/service/ec2 v1.173.0
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.30.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.15 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.17 // indirect
+	github.com/aws/smithy-go v1.20.3 // indirect
+)
--- aws_unused_resources/unused_ebs.go
+++ aws_unused_resources/unused_ebs.go
@@ -1,35 +1,8 @@
-package main
+package aws_unused_resources
 
 import (
-	"context"
-	"fmt"
-	"log"
-	// "encoding/json"
-
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
-	// "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 )
-
-func main() {
-	// Load the shared AWS configuration.
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithRegion("us-east-1"),
-		// replace the "limited-admin" with the profile of your choice or completely remove that when running in a Lambda Environment
-		config.WithSharedConfigProfile("limited-admin"),
-	)
-	if err != nil {
-		log.Fatalf("Unable to load AWS SDK config: %v", err)
-	}
-
-	unused_ebs_data := get_unused_ebs_volumes(cfg)
-	fmt.Printf("Unused EBS Volume IDs %s\nTotal Volume Count %d\nUnused Count: %d", 
-		unused_ebs_data.ResourceIDs, 
-		unused_ebs_data.TotalInstancesCount,
-		unused_ebs_data.UnusedInstancesCount,
-	)
-}
 
 type UnusedResourceMetrics struct {
 	ResourceIDs []string
