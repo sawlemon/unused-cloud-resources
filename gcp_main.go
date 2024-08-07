ed352870e49b8b152ef443f6828cc1a63f54388c
solomonraj-a-presidio <solomonraj.a@presidio.com>
2024-08-07 23:29:32 +0530

    gcp bucket listing package poc completed

--- gcp_main.go
+++ gcp_main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
 	gcp_unused "github.com/sawlemon/unused-cloud-resources/gcp_unused_resources"
 )
 
 func main() {
-	fmt.Println(gcp_unused.Sample())
+	gcp_unused.AuthenticateImplicitWithAdc("root-fort-431811-j3")
 }
